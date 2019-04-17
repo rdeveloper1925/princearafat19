a23eca765439cb1bb2e26961db865705fc1cfb70
rdeveloper1925 <rdeveloper1925@gmail.com>
2019-04-17 19:58:48 +0300

    First functional

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import './bootstrap.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      articles:[]
     };
   }
 
+  componentDidMount(){
+    axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d34d2662b6c14fd9bc90399cb5ba8f33')
+    .then((response)=>{
+      this.setState({articles:response.data.articles});
+      console.log(response.data.articles);
+    })
+    .catch();
+  }
+
   render() {
+    let style={'width':'18rem'};
+
+    const articles=this.state.articles.map((v,i)=>{
+      return (
+        <div className="card" id={i} style={style}>
+          <img className="card-img-top" src={v.urlToImage} alt="Image Loading"/>
+          <div className="card-body">
+            <h5 className="card-title">{v.author}</h5>
+            <h6 className="card-title">{v.title}</h6>
+            <p className="card-text">{v.description}</p>
+            <a href={v.url} className="btn btn-primary btn-sm">Read More</a><br/>
+            <small><b>{v.source.name}</b></small>
+          </div>
+        </div>
+      );
+    })
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        //{articles}
       </div>
     );
   }
